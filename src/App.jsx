7af741e300bd7988e575d2318da94b1a901b9ade
sabiq7392 <sabiqmuhammad@gmail.com>
2021-12-22 21:39:08 +0700

    start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Maps from './components/maps';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <main>
+        <Maps />
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
