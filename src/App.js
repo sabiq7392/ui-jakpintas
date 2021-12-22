3d4de8b2c4c1641d99b905323543fafca3c36617
sabiq7392 <sabiqmuhammad@gmail.com>
2021-12-22 20:05:20 +0700

    add photo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import sabiq from './images/sabiq.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={sabiq} className="App-logo" alt="logo" />
         <p>
           Hai JakPintas
         </p>
